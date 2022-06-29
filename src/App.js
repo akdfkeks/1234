416077cdc96e6d4dbaf02b6cbb7370d831881cf3
yeaeun21 <yeaeun28@naver.com>
2022-06-29 14:31:53 +0900

    login_success_router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
 import Main from './pages/main/main';
-
+import Login from './pages/login/Login';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Main />
-        
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path='main' element={<Main/>}/>
+        </Routes>
+      </Router>
     );
   }
 }
