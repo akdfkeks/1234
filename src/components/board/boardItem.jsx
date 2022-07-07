7ed8d129a1e309dae9f9bb500508ef282f45a43a
akdfkeks <akdfkeks@gmail.com>
2022-07-07 14:09:55 +0900

    .

--- src/components/board/boardItem.jsx
+++ src/components/board/boardItem.jsx
@@ -1,33 +1,34 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 export default class BoardItem extends Component {
-    checkItem() {
-        const checkComponent = document.querySelector("#board-item" + this.props.id);
-        if (checkComponent.style.cssText) {
-            checkComponent.style.cssText = 0;
-        } else {
-            checkComponent.style.cssText = "text-decoration: line-through";
-        }
-    }
-    
-    render() {
-        return (
-            <li id={"board-item"+this.props.id}>
-                <input 
-                type="button" 
-                value="✅" 
-                onClick={()=>{
-                    this.checkItem()}}
-                />
-                {this.props.text}
-                <input
-                    type="button"
-                    value="X"
-                    onClick={() => {
-                        this.props.delete(this.props.id);
-                    }}
-                />
-            </li>
-        );
-    }
+	checkItem() {
+		const checkComponent = document.querySelector("#board-item" + this.props.id);
+		if (checkComponent.style.cssText) {
+			checkComponent.style.cssText = 0;
+		} else {
+			checkComponent.style.cssText = "text-decoration: line-through";
+		}
+	}
+
+	render() {
+		return (
+			<li id={"board-item" + this.props.id}>
+				<input
+					type="button"
+					value="✅"
+					onClick={() => {
+						this.checkItem();
+					}}
+				/>
+				{this.props.text}
+				<input
+					type="button"
+					value="X"
+					onClick={() => {
+						this.props.delete(this.props.id);
+					}}
+				/>
+			</li>
+		);
+	}
 }
--- src/components/board/boardList.js
+++ src/components/board/boardList.js
@@ -1,67 +1,72 @@
 import Board from "./board";
 import BoardItem from "./boardItem";
-import { Component } from 'react';
-import axios from 'axios';
+import { Component } from "react";
+import axios from "axios";
 
 export default class BoardList extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            itemNum: 0,
-            items: [],
-            homeAddr:"172.30.17.114:3001"
-        };
-    }
+		this.state = {
+			itemNum: 0,
+			items: [],
+			homeAddr: "akdfkeks.iptime.org:3001",
+		};
+	}
 
-    deleteItem(num) {
-        const selected = document.querySelector('#board-item'+num);
-        if (selected) {
-            selected.remove();
-        }
-    }
+	deleteItem(num) {
+		const selected = document.querySelector("#board-item" + num);
+		if (selected) {
+			selected.remove();
+		}
+	}
 
-    addItem() {
-        const inputText = document.querySelector('#inputText');
-        if (inputText.value){
-            const tempArr = [...this.state.items];
-            tempArr.push(
-            <BoardItem
-                //id={this.state.itemNum++}
-                id={this.setState({itemNum: this.state.itemNum+1})}
-                text={inputText.value}
-                delete={(num)=>{
-                    this.deleteItem(num)}}
-                    />
-            );
-            this.setState({
-                items: tempArr
-            })
-            axios({
-                method:'post',
-                url:`http://${this.state.homeAddr}/user/board`,
-                data:{
-                    bordTitle:this.setState.items
-                }
-            }).then(Response=>{console.log(Response)})
-            
-            inputText.value=""
-        }
-    }
+	addItem() {
+		const inputText = document.querySelector("#inputText");
+		if (inputText.value) {
+			const tempArr = [...this.state.items];
+			tempArr.push(
+				<BoardItem
+					//id={this.state.itemNum++}
+					id={this.setState({ itemNum: this.state.itemNum + 1 })}
+					text={inputText.value}
+					delete={(num) => {
+						this.deleteItem(num);
+					}}
+				/>
+			);
+			this.setState({
+				items: tempArr,
+			});
+			axios({
+				method: "post",
+				url: `http://${this.state.homeAddr}/user/board`,
+				data: {
+					boardTitle: this.setState.items,
+				},
+			}).then((Response) => {
+				console.log(Response);
+			});
 
-    render() {
-        return (
-            <div className="BoardName">
-                <input 
-                autoComplete="off"
-                id="inputText"
-                type="text"
-                placeholder="게시판 이름">
-                </input>
-                <input className="AddButton" type="button" value="↩" name="boardTitle" onClick={()=>{ this.addItem() }}
-                />
-                <Board items={this.state.items}/>
-            </div>
-        );
-    }
+			inputText.value = "";
+		}
+	}
+
+	render() {
+		return (
+			<div className="BoardName">
+				<input autoComplete="off" id="inputText" type="text" placeholder="게시판 이름"></input>
+				<input
+					className="AddButton"
+					type="button"
+					value="↩"
+					name="boardTitle"
+					onClick={() => {
+						this.addItem();
+					}}
+				/>
+				<Board items={this.state.items} />
+			</div>
+		);
+	}
 }
