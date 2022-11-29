340b27897e95da40f4947c82e34b9ff3399f193b
leehh6024 <dlgusghchlrh@gmail.com>
2022-11-30 00:07:54 +0900

    .

--- src/components/calendar/MainCalendar.js
+++ src/components/calendar/MainCalendar.js
@@ -1,6 +1,8 @@
 import '../../App.css';
 import React, {useState} from 'react';
 import Calendar from 'react-calendar';
+import { array } from 'prop-types';
+// import moment from 'moment';
 //import AddToDo from '../todolist/addToDo';
 //import moment from 'moment';
 // import axios from 'axios';
@@ -13,24 +15,36 @@ function MainCalendar() {
   const [value, onChange] = useState(new Date());
   //  const [schedule, setSchedule] = useState("");
 
-  //하이라이트가 들어갈 날짜를 배열로 작성
-  // const marks = [1
-  //   "2022-05-15",
-  //   "2022-05-03",
-  //   "2022-05-07",
-  //   "2022-05-12",
-  // ]; 
-
   const onActiveStartDateChange = ({ action, activeStartDate, value, view }) => {
-  // alert('Changed view to: ', activeStartDate, view)
     document.getElementById("amu").textContent = activeStartDate
-  };
   
-  // const onClickDay = (value, event) => {
-  //   document.getElementById("amu").textContent = value
-  // };
+    let FullDate = [];
 
+    const yearMonth = FullDateasdf
+
+    for (var i = 0; i<8; i++) {
+      FullDate.concat(new array());
+    }
+
+
+
+
+    activeStartDate;
     
+    // console.log(yearMonth);
+
+    // document.writeln(yearMonth[0]);
+    // document.writeln(yearMonth[1]);
+    // document.writeln(yearMonth[2]);
+    // document.writeln(yearMonth[3]);
+  };
+
+ 
+
+  //  const onClickDay = (value, event) => {
+  //    document.getElementById("amu").textContent = value
+  //  };
+
   return (
     <div>
       
@@ -42,13 +56,19 @@ function MainCalendar() {
         maxDetail="month"
         // onClickDay={onClickDay}
         // onClickMonth={onClickMonth}
+        // view={onActiveStartDateChange}
+        
         value={value}/>
         
         <h1 id="amu">
 
         </h1>
-      
+        <div>
+
+        </div>
     </div>
+
+    
     
   );
 
@@ -81,3 +101,13 @@ export default MainCalendar;
   //     calendar[schedule.targetDate.split("T")[0].slice(-2)-1].concat(schedule);
   //   })
   // })
+
+
+
+  //하이라이트가 들어갈 날짜를 배열로 작성
+  // const marks = [1
+  //   "2022-05-15",
+  //   "2022-05-03",
+  //   "2022-05-07",
+  //   "2022-05-12",
+  // ]; 
